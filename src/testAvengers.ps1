aa6d8a66ffb2d97089e17260e653862d21170126
GastonVidart <gaston.vidart@gmail.com>
2020-04-17 18:56:47 -0300

    Inicio v3

---
+++ src/testAvengers.ps1
@@ -0,0 +1,5 @@
+﻿Write-Host "Inicio RMI"
+java -Djava.security.policy=C:\Users\gasty\Desktop\D_Ej1\Sservidor\servidor.permisos ServidorEco 127.0.0.1 54321
+java -Djava.security.policy=C:\Users\gasty\Desktop\D_Ej1\Cliente\cliente.permisos  ClienteEco localhost 54321 funciona
+Write-Host "RMI Iniciado - Ejecute Cliente"
+
