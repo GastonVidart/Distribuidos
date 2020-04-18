4628453af91cf05eb0d5e59a75c2910b36899812
guido <guido@192.168.0.175>
2020-04-18 19:44:45 -0300

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- src/D_Ej2cliente/Cliente.java
+++ src/D_Ej2cliente/Cliente.java
@@ -4,7 +4,7 @@ public class Cliente {
     public Cliente() 
     {
         try {		
-            Calculadora calc = (Calculadora)Naming.lookup ("//localhost:5000/CalculadoraImp");
+            Calculadora calc = (Calculadora)Naming.lookup ("//localhost:54321/CalculadoraImp");
             System.out.print ("2 + 3 = "); System.out.println (calc.suma(2,3));
 	    System.out.print ("7 - 3 = "); System.out.println (calc.resta(7,3));
 	    System.out.print ("9 / 3 = "); System.out.println (calc.div(9,3));
