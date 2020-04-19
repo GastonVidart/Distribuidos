a9c4202b5b818a9f5bf57d88b0271ca28706c62c
GastonVidart <gaston.vidart@gmail.com>
2020-04-18 21:14:07 -0300

    Templates

---
+++ src/TP2Distribuidos/ServerHoroscopo.java
@@ -0,0 +1,9 @@
+package TP2Distribuidos;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ServerHoroscopo extends Remote {
+
+    public String getHoroscopo(String horoscopo) throws RemoteException;
+}
