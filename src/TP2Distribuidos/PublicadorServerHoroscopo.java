89a0a804e9b5da7ef8b627766b87fd342e147810
yophex <seba_yaupe@hotmail.com>
2020-04-22 00:05:44 -0300

    Merge origin/master
    Conflicts:
    	src/Player.java

---
+++ src/TP2Distribuidos/PublicadorServerHoroscopo.java
@@ -0,0 +1,32 @@
+
+package TP2Distribuidos;
+
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+
+
+public class PublicadorServerHoroscopo {
+    private static final String ip = "127.0.0.1", puerto = "10001";
+
+    public static void main(String[] args) {
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+            System.setProperty("java.rmi.server.hostname", "localhost");
+        }
+        try {
+            ServerClima serverClima = new ServerClimaImp();
+            Naming.rebind("rmi://" + ip + ":" + puerto + "/ServerClimaAA", serverClima);
+            System.out.println("Servidor Clima Publicado");
+        } catch (RemoteException e) {
+            System.err.println("Error de comunicacion: " + e.toString());
+            System.exit(1);
+        } catch (Exception e) {
+            System.err.println("Excepcion en ServidorEco:");
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+    
+    
+    
+}
